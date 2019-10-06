b9aef4f7a07a9cc623cac9e9e3bb3c2020962c50
snext1220 <yoshihiro@wings.msn.to>
2019-10-06 15:39:38 +0900

    Add: Command

--- playground/lib/playground.js
+++ playground/lib/playground.js
@@ -1895,6 +1895,8 @@ $(function () {
       'white': '%white%$0%/%',
       '*ruby': '${text|ruby}',
       'if': '${if condition}$0${/if}',
+      '*import': '${import 99999}',
+      '*input': '${input?0}',
       '*input': '${input?0}',
       '*title': '${title}',
       '*race': '${race?FIG:WIZ:DWA:ELF}',
@@ -1904,6 +1906,7 @@ $(function () {
       '*random': '${rand?min:max}',
       '*msg': '${msg?str1:str2:...}',
       'tweet': '${tweet}%0%${/tweet}',
+      '*capture': '![caption](path)',
     };
     let comm = command[$(this).data('command')];
     if (comm.startsWith('*')) {
