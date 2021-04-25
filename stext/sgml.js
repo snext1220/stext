0e47eb2a8eff6919e30f013764354125e4a3bb28
YAMADA Yoshihiro <CQW15204@nifty.com>
2021-04-26 00:53:57 +0900

    Add: RpgDice

--- stext/sgml.js
+++ stext/sgml.js
@@ -4487,6 +4487,97 @@
     }
   };
 
+  class RpgDice {
+    // target：ダイスの反映先
+    // type：ダイス型（数値：1～n、文字列：dicesetのキー）
+    // num：ダイスの個数（2～5）
+    constructor(target, type = 6 , num = 2) {
+      // カスタムのダイス定義
+      this.diceset = {
+        'high' : [4, 5, 6],
+        'cheat_l': [1, 1, 1, 2, 3, 4, 5, 6],
+        'cheat_h': [1, 2, 3, 4, 5, 6, 6, 6],
+      };
+      // ダイスの型
+      this.type = type;
+      // ダイスを反映させる先（セレクター式）
+      this.target = target;
+      // ダイスの個数
+      this.num = num;
+      // ダイスの現在値（初期値はすべて1）
+      this.current = new Array(this.num);
+      this.current.fill(1);
+      // ダイスの回転数（内部用途）
+      this.rotateCount = 1;
+    }
+
+    // ダイスのためのHTMLを生成（private）
+    getHtml() {
+      let html = '';
+      for (let i = 0; i < this.num; i++) {
+        // ダイスの値を保管
+        if (Array.isArray(this.type)) {
+            // TODO
+        } else {
+          this.current[i] = Util.random(1, this.type);
+        }
+        html += `<img src="${ROOT}${COMMON}cube${this.current[i]}.png" class="dice" />`;
+      }
+      return html;
+    }
+
+    // 1回分のローテート（private）
+    rotateOne(that) {
+      that.rotateCount++;
+      $(that.target).html(that.getHtml());
+      if(that.rotateCount > 20) { return; }
+      setTimeout(that.rotateOne, 50, that);
+    }
+
+    // ダイスを振る
+    rotate() {
+      //SeAudio.play('dice', true);
+      this.rotateCount = 1;
+      this.rotateOne(this);
+    }
+
+    // 出目を取得
+    getRoll(key) {
+      switch(this.num) {
+        case 2:
+          if (key === 'L') { return this.current[0]; }
+          if (key === 'R') { return this.current[1]; }
+          break;
+        case 3:
+          if (key === 'L') { return this.current[0]; }
+          if (key === 'M') { return this.current[1]; }
+          if (key === 'R') { return this.current[2]; }
+          break;
+        case 4:
+          if (key === 'L')  { return this.current[0]; }
+          if (key === 'LM') { return this.current[1]; }
+          if (key === 'RM') { return this.current[2]; }
+          if (key === 'R')  { return this.current[3]; }
+          break;
+        case 5:
+          if (key === 'L')  { return this.current[0]; }
+          if (key === 'LM') { return this.current[1]; }
+          if (key === 'M')  { return this.current[2]; }
+          if (key === 'LR') { return this.current[3]; }
+          if (key === 'R')  { return this.current[4]; }
+          break;
+      }
+      return 0;
+    }
+  }
+  // テストコード
+  // let d = new RpgDice('#test_result', 6, 2);
+  // d.rotate();
+  // $(document).on('click', '#test_result', function(e) {
+  //   console.log('TEST');
+  //   d.rotate();
+  // });
+
   // プラグイン本体
   // 引数code：シナリオコード、またはシナリオ文字列
   // 引数debug：デバッグウィンドウを表示するか（既定はfalse）
